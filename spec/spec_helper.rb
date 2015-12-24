366f234c7748e7f28a36d36867da0b41da5018e3
Tom Johnson <tom@dp.la>
2015-12-23 20:14:27 -0800

    Skip benchmarks by default in RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,7 @@ require 'hexagraph'
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
-  config.exclusion_filter = {ruby: lambda { |version|
-    RUBY_VERSION.to_s !~ /^#{version}/
-  }}
+  config.filter_run_excluding benchmark: true
 end
 
 Encoding.default_external = Encoding::UTF_8
